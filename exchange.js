fe9feeea0f17054ddd4d54d34fb44d9e58b2d6e2
Sarah Oluwasami <saraholuwasami43@gmail.com>
2022-11-03 12:53:24 +0100

    exchangerate

--- exchange.js
+++ exchange.js
@@ -1,35 +1,40 @@
 
 
-function submit(){
-    let input = document.getElementById('select')
-    let inputValue = input.value
-        axios(`https://v6.exchangerate-api.com/v6/cc02bc7972a0c8b71e9ac68b/latest/${inputValue}`)
-        .then(serverData =>{
-            let rate = serverData.data.conversion_rates
-            let conversion = JSON.stringify(rate);
-            console.log(typeof (conversion))
-
-            let newTable =  `<table>
-            <tr>
-                <th>Currency Code</th>
-                <th>Conversion Rates</th>
-            </tr>`
-            for(let value of conversion){
-               newTable += `<tr> <td> ${value.aed} </td> <td> ${value.afn} </td> <tr>`
+   let h1doc = document.querySelector('form');
+   h1doc.classList.add('frame')
+ function submit1(){
+let h1doc = document.querySelector('form');
+    h1doc.classList.remove('frame');
+    h1doc.classList.add('frame1');
+             document.getElementById('text').innerHTML= `<p id="pp"><b>Currency</b></p> <p id="pp"><b>Rate</b></p>`
+let select = document.getElementById('select');
+let rate = select.value;
+    axios(`https://v6.exchangerate-api.com/v6/1686e513b42112194982dddb/latest/${rate}`)
+    .then(serverData =>{
+        let ratee = serverData.data;
+        let conv = ratee.conversion_rates;
+            for (let key in conv){
+                if (key){
+                    document.getElementById('out').innerHTML += key  + '<br></br>';
+                    
+                }
             }
-            newTable += "</table>";
-            document.getElementById("rate").innerHTML = newTable;
-
-        // document.getElementById('rate2').innerHTML=conversion2 +'<br>'
-        // document.getElementById('rate1').innerHTML=conversion1
-
-        console.log(serverData.data);
+            for (let i in conv){
+                if (i){
+                    document.getElementById('outt').innerHTML += conv[i] + '<br></br>';
+                    
+                }
+            }
+            
+            console.log(conv)
         }).catch(err =>{
             console.log(err)
         })
 }
 
-function submit1(){
+    
+
+function submit(){
     let input = document.getElementById('first-code')
         inputValue = input.value
     let output = document.getElementById('second-code')
@@ -41,10 +46,10 @@ function submit1(){
         `)
         .then(serverData =>{
         let rate = serverData.data
-        let rate1 = rate.conversion_result + " " + rate.conversion_rate
-        let conversion = JSON.stringify(rate1);
-        console.log(typeof (conversion))
-        document.getElementById('display').innerHTML=conversion
+        let rate1 = rate.conversion_result
+        let rate2 = rate.conversion_rate
+        document.getElementById('display1').innerHTML=rate1
+        document.getElementById('display2').innerHTML=rate2
         console.log(serverData.data)
         }).catch(err =>{
             console.log(err)
